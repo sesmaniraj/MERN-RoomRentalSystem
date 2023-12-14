bb384821d69e4877d4ac15bd45a393eb7232a4cf
AbhishekAle <aviale017@gmail.com>
2023-12-14 16:11:34 +0545

    component updated: Admin Dashboard Created

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Register from "./components/auth/Register";
 import HomePage from "./components/pages/HomePage";
 import Login from "./components/auth/Login";
+import AdminDashboard from "./components/dashboard/AdminDashboard";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Route path="/" element={<HomePage />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/admindashboard" element={<AdminDashboard />} />
     </Routes>
   );
 };
--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
     <div>
       <Link to={"/login"}>Login</Link>
       <Link to={"/register"}>Register</Link>
+      <Link to={"/admindashboard"}>Dashboard</Link>
     </div>
   );
 };
