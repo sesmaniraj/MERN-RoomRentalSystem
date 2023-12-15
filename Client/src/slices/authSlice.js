6da3b07fd8aabfa74958fc9ef7e0aef2868fbb3c
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-15 22:21:43 +0545

    [update]:Reduxtoolkit setup for user login logout and register done.

--- Client/src/slices/authSlice.js
+++ Client/src/slices/authSlice.js
@@ -1,3 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const authSlice = createSlice({});
+const initialState = {
+  userInfo: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setCrendentials: (state, action) => {
+      state.userInfo = action.payload;
+      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+    },
+  },
+});
+export const { setCrendentials } = authSlice.actions;
+export default authSlice.reducer;
---
+++ Client/src/slices/userSlice.js
@@ -0,0 +1,28 @@
+import { apiSlice } from "./apiSlice";
+
+const USERS_URL = "/api/v1";
+
+export const userApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/register`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/logout`,
+        method: "POST",
+      }),
+    }),
+  }),
+});
