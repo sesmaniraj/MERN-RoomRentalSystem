a06fa0d80b3714fce1abeb230f23a895449d5b32
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-17 10:58:22 +0545

    update: Room controller for getting room and room by id created.

--- Server/controllers/roomController.js
+++ Server/controllers/roomController.js
@@ -22,3 +22,33 @@ export const registerRoom = async (req, res) => {
     res.status(401).json({ error });
   }
 };
+
+export const getRoom = async (req, res) => {
+  try {
+    const rooms = await RoomModel.find();
+    res.status(201).json({ rooms });
+  } catch (error) {
+    res.status(404).json({ message: "Room not found" });
+  }
+};
+
+export const getRoomById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({ message: "Invalid Room ID" });
+    }
+
+    const room = await RoomModel.findById(id);
+
+    if (!room) {
+      return res.status(404).json({ message: "Room not found" });
+    }
+
+    res.status(200).json({ room });
+  } catch (error) {
+    console.error("Error fetching room by ID:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- Server/routes/roomRoute.js
+++ Server/routes/roomRoute.js
@@ -1,7 +1,13 @@
 import express from "express";
-import { registerRoom } from "../controllers/roomController.js";
+import {
+  getRoom,
+  getRoomById,
+  registerRoom,
+} from "../controllers/roomController.js";
 const roomRouter = express.Router();
 
 roomRouter.post("/registerroom", registerRoom);
+roomRouter.get("/getroom", getRoom);
+roomRouter.get("/getroom/:id", getRoomById);
 
 export default roomRouter;
