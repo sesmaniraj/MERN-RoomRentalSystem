42e9a223daf109bcc473c9630f4412ac29cd58cf
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-21 13:09:18 +0545

    Image upload of room done with firebase

--- Client/src/components/pages/RoomRegister.jsx
+++ Client/src/components/pages/RoomRegister.jsx
@@ -1,16 +1,78 @@
+import {
+  getStorage,
+  uploadBytesResumable,
+  ref,
+  getDownloadURL,
+} from "firebase/storage";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { app } from "../../firebase.js";
 
 const RoomRegister = () => {
-  const [formData, setFormData] = useState("");
-  const navigate = useNavigate();
   const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  });
+  const [imageError, setImageError] = useState(false);
+  console.log(formData);
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value });
-    console.log(formData);
+  // const handleChange = (e) => {
+  //   setFormData({ ...formData, [e.target.id]: e.target.value });
+  //   console.log(formData);
+  // };
+  const handleSubmitImage = (e) => {
+    console.log("hell0");
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      const promises = [];
+
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setImageError(false);
+        })
+        .catch((err) => {
+          setImageError("Image upload failed (2mb max)");
+        });
+    } else {
+      setImageError("You can only upload 6");
+    }
   };
-  const handleSubmitImage = () => {};
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`upload ${progress} %`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+  const handleDeleteImage = (index) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i != index),
+    });
+  };
   const submitHandler = async () => {
     e.preventDefault();
   };
@@ -26,7 +88,7 @@ const RoomRegister = () => {
       >
         <input
           id="name"
-          onChange={handleChange}
+          // onChange={handleChange}
           type="text"
           placeholder="Enter Name"
           className="border-solid border-2 p-1.5 border-sky-500 outline-none rounded-md"
@@ -34,14 +96,14 @@ const RoomRegister = () => {
         />
         <textarea
           id="description"
-          onChange={handleChange}
+          // onChange={handleChange}
           type="text"
           placeholder="Description"
           className="border-solid border-2 p-1.5 border-sky-500 outline-none rounded-md"
         />
         <input
           id="address"
-          onChange={handleChange}
+          // onChange={handleChange}
           type="text"
           placeholder="Address"
           className="border-solid border-2 p-1.5 border-sky-500 outline-none rounded-md"
@@ -102,14 +164,33 @@ const RoomRegister = () => {
             accept="image/*"
             multiple
             className="p-3 border border-gray-300 rounded-lg"
-            onChange={(e) => setFiles(e.target.value)}
+            onChange={(e) => setFiles(e.target.files)}
           />
           <button
+            type="button"
             onClick={handleSubmitImage}
             className="bg-slate-400 rounded-md p-2 m-10"
           >
             Upload Image
           </button>
+          <p className="text-red-700">{imageError && imageError}</p>
+          {formData.imageUrls.length > 0 &&
+            formData.imageUrls.map((url, index) => (
+              <div key={url} className="flex">
+                <img
+                  src={url}
+                  alt=""
+                  className="w-30 h-30 object-contain rounded-lg "
+                />
+                <button
+                  type="button"
+                  className="text-red-700"
+                  onClick={() => handleDeleteImage(index)}
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
         </div>
         <button type="submit" className="bg-sky-400 rounded-md p-2">
           Register Room
