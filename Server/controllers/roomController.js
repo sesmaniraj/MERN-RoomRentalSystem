bad74f7ae234ef1b741181a5c3454e3a28336503
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-22 12:02:40 +0545

    Delete room api route created

--- Server/controllers/roomController.js
+++ Server/controllers/roomController.js
@@ -21,3 +21,18 @@ export const getUserRoom = async (req, res, next) => {
     return next(errorHandler(401, "Only can see your own room"));
   }
 };
+export const deleteRoom = async (req, res, next) => {
+  const room = await RoomModel.findById(req.params.id);
+  if (!room) {
+    return next(errorHandler(404, "Room not found"));
+  }
+  if (req.user.id != room.userRef) {
+    return next(errorHandler(401, "you can only delete your room"));
+  }
+  try {
+    await RoomModel.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Room has been deleted" });
+  } catch (error) {
+    next(error);
+  }
+};
--- Server/routes/roomRoute.js
+++ Server/routes/roomRoute.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { registerRoom, getUserRoom } from "../controllers/roomController.js";
+import {
+  registerRoom,
+  getUserRoom,
+  deleteRoom,
+} from "../controllers/roomController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const roomRouter = express.Router();
 
 roomRouter.post("/registerroom", verifyToken, registerRoom);
 roomRouter.get("/room/:id", verifyToken, getUserRoom);
+roomRouter.delete("/deleteroom/:id", verifyToken, deleteRoom);
 
 export default roomRouter;
