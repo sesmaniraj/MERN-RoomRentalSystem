7e91dd9fa0b9ed9dafef5282ab5def5f23a1d8bd
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-28 00:49:56 +0545

    Listing item css updated

--- Client/src/components/ListingItem.jsx
+++ Client/src/components/ListingItem.jsx
@@ -1,10 +1,11 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import { MdLocationOn } from "react-icons/md";
 
-export default function ListingItem({ listing }) {
+const ListingItem = ({ listing }) => {
   return (
     <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
-      <Link to={`/listing/${listing._id}`}>
+      <Link to={`/listing/${listing._id}`} className="block">
         <img
           src={
             listing.imageUrls[0] ||
@@ -13,40 +14,36 @@ export default function ListingItem({ listing }) {
           alt="listing cover"
           className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
         />
-        <div className="p-3 flex flex-col gap-2 w-full">
-          <p className="truncate text-lg font-semibold text-slate-700">
+        <div className="p-4">
+          <h2 className="text-lg font-semibold text-slate-700 truncate">
             {listing.name}
-          </p>
-          <div className="flex items-center gap-1">
+          </h2>
+          <div className="flex items-center text-gray-600 text-sm mb-2">
             <MdLocationOn className="h-4 w-4 text-green-700" />
-            <p className="text-sm text-gray-600 truncate w-full">
-              {listing.address}
-            </p>
+            <p className="truncate ml-1">{listing.address}</p>
           </div>
-          <p className="text-sm text-gray-600 line-clamp-2">
+          <p className="text-gray-600 text-sm line-clamp-2 mb-4">
             {listing.description}
           </p>
-          <p className="text-slate-500 mt-2 font-semibold ">
+          <p className="text-slate-500 font-semibold ">
             $
             {listing.offer
               ? listing.discountPrice.toLocaleString("en-US")
               : listing.regularPrice.toLocaleString("en-US")}
             {listing.type === "rent" && " / month"}
           </p>
-          <div className="text-slate-700 flex gap-4">
-            <div className="font-bold text-xs">
-              {listing.bedrooms > 1
-                ? `${listing.bedrooms} beds `
-                : `${listing.bedrooms} bed `}
+          <div className="flex text-slate-700 gap-4 text-xs">
+            <div>
+              {listing.bedrooms} {listing.bedrooms > 1 ? "beds" : "bed"}
             </div>
-            <div className="font-bold text-xs">
-              {listing.bathrooms > 1
-                ? `${listing.bathrooms} baths `
-                : `${listing.bathrooms} bath `}
+            <div>
+              {listing.bathrooms} {listing.bathrooms > 1 ? "baths" : "bath"}
             </div>
           </div>
         </div>
       </Link>
     </div>
   );
-}
+};
+
+export default ListingItem;
