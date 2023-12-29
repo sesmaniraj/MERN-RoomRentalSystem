c0de65b95483f39b9b43cb7c19fe7f2114a548bd
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-29 18:11:48 +0545

    Controller created for booking api

---
+++ Server/controllers/bookingController.js
@@ -0,0 +1,56 @@
+// controllers/bookingController.js
+import bookingModel from "../models/bookingModel.js";
+
+// Get all bookings
+export const getBookings = async (req, res) => {
+  try {
+    const bookings = await BookingModel.find();
+    res.json(bookings);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get a single booking by ID
+export const getBookingById = async (req, res) => {
+  try {
+    const booking = await BookingModel.findById(req.params.id);
+    res.json(booking);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Create a new booking
+export const createBooking = async (req, res) => {
+  try {
+    const booking = await BookingModel.create(req.body);
+    res.status(201).json(booking);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Update a booking by ID
+export const updateBooking = async (req, res) => {
+  try {
+    const booking = await BookingModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.json(booking);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Delete a booking by ID
+export const deleteBooking = async (req, res) => {
+  try {
+    await BookingModel.findByIdAndDelete(req.params.id);
+    res.json({ message: "Booking deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
