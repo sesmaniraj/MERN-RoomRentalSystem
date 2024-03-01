589504765f475e06bc869b87dc4594950b3a9643
sesmaniraj <sesmaniraj7@gmail.com>
2024-03-01 10:41:05 +0545

    updated

--- Client/src/components/pages/Home.jsx
+++ Client/src/components/pages/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
   useEffect(() => {
     const fetchOfferListings = async () => {
       try {
-        const res = await fetch("/api/v1/get?offer=true&limit=4");
+        const res = await fetch("/api/v1/getallroom");
         const data = await res.json();
         setOfferListings(data);
       } catch (error) {
@@ -47,7 +47,7 @@ export default function Home() {
       </div>
 
       {/* Listing Results Section */}
-      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
+      <div className="mb-5">
         {offerListings.length > 0 && (
           <div>
             <div className="my-3">
@@ -61,7 +61,7 @@ export default function Home() {
                 Search more here
               </Link>
             </div>
-            <div className="flex gap-2 mx-auto">
+            <div className="flex  flex-wrap  gap-2 ">
               {offerListings.map((listing) => (
                 <ListingItem listing={listing} key={listing._id} />
               ))}
--- Client/src/components/pages/RecommendedRoom.jsx
+++ Client/src/components/pages/RecommendedRoom.jsx
@@ -4,15 +4,18 @@ import { Navigation } from "swiper/modules";
 import SwiperCore from "swiper";
 import "swiper/css/bundle";
 import ListingItem from "../ListingItem";
+import { useSelector } from "react-redux";
 
 const RecommendedRoom = () => {
   const [offerListings, setOfferListings] = useState([]);
   SwiperCore.use([Navigation]);
 
+  const currentUser = useSelector((state) => state.user.currentUser);
+  console.log(currentUser._id);
   useEffect(() => {
     const fetchOfferListings = async () => {
       try {
-        const res = await fetch("/api/v1/get?offer=true&limit=4");
+        const res = await fetch(`/api/v1/getbyuserdata/${currentUser._id}`);
         const data = await res.json();
         setOfferListings(data);
       } catch (error) {
@@ -24,7 +27,7 @@ const RecommendedRoom = () => {
 
   return (
     <div>
-      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
+      <div className="mt-3">
         {offerListings.length > 0 && (
           <div>
             <div className="my-3">
